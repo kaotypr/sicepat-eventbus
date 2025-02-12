6b8e0385ac450acaef20d6ebabffa3ee745aa343
adhitya yosua sanjaya <adhityasnjy@gmail.com>
2025-02-12 12:39:13 +0700

    chore: separate 'dev' script tsup config - this separation tsupconfig handles issue when splitting code is making a new chunk files each re-build on watch mode

---
+++ tsup.dev.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: {
+    index: 'src/index.ts',
+    hooks: 'src/hooks.ts',
+  },
+  format: ['cjs', 'esm'],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: false,
+  treeshake: true,
+  external: ['react', 'react-dom'],
+  watch: true,
+});
